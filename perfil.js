4f5723a88d915a2133e4e9c2e4a7020ccd6838ea
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 03:16:49 -0300

    Create perfil.js

---
+++ perfil.js
@@ -0,0 +1,17 @@
+import { updateProfile } from "./profiles.js";
+
+document.getElementById("saveProfile").onclick = async () => {
+  const displayName = document.getElementById("displayNameInput").value;
+  const bio = document.getElementById("bioInput").value;
+  const avatarURL = document.getElementById("avatarInput").value;
+  const bannerURL = document.getElementById("bannerInput").value;
+
+  await updateProfile({
+    displayName,
+    bio,
+    avatarURL,
+    bannerURL
+  });
+
+  alert("Perfil atualizado!");
+};
