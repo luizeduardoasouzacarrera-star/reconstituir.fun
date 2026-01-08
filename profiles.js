d13d7c89aebea48e354196d286d36dfafbb7daf7
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 20:15:51 -0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -10,7 +10,7 @@ const socialIcons = {
   tiktok: "https://cdn.worldvectorlogo.com/logos/tiktok-icon-2.svg",
   valorant: "https://www.svgrepo.com/show/424912/valorant-logo-play-2.svg",
   steam: "https://img.icons8.com/ios11/512/FFFFFF/steam-circled.png",
-  twitter: "https://img.freepik.com/free-vector/new-twitter-logo-x-icon-black-background_1017-45427.jpg",
+  twitter: "https://cdn.freelogovectors.net/wp-content/uploads/2023/07/x-logo-twitter-freelogovectors.net_.png",
   spotify: "https://upload.wikimedia.org/wikipedia/commons/a/a1/2024_Spotify_logo_without_text_%28black%29.svg"
 };
 
