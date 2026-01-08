cca1947e5c1978de27e3cf433ae99167d24098ee
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 01:02:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } 
   from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Garantir que o HTML já carregou
 document.addEventListener("DOMContentLoaded", () => {
-
-  const msg = document.getElementById("msg");
   const loginBtn = document.getElementById("loginBtn");
+  const msg = document.getElementById("msg");
 
   loginBtn.addEventListener("click", () => {
     const username = document.getElementById("username").value.trim();
@@ -18,17 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    // Converte o nome em um "email fake" para o Firebase
     const emailFake = `${username.toLowerCase()}@reconstituir.fun`;
 
     signInWithEmailAndPassword(auth, emailFake, password)
       .then(() => {
-        // 🔥 REDIRECIONA PARA A TELA DE SUCESSO
+        // 🔥 REDIRECIONA PARA OUTRA PÁGINA
         window.location.href = "dashboard.html";
       })
       .catch(() => {
         msg.innerText = "❌ Nome ou senha inválidos";
       });
   });
-
 });
