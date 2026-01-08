1e9a353e761c086368659619967dffda748c7b25
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 04:47:00 -0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -10,7 +10,7 @@ onSnapshot(collection(db, "profiles"), snapshot => {
 
   snapshot.forEach(doc => {
     const data = doc.data();
-
+    
     profiles.innerHTML += `
       <div class="profile-card">
         <strong>${data.username}</strong>
