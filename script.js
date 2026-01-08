8a2d8d3f6a7fd3c913387b4409f728ec158b908f
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 00:45:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const msg = document.getElementById("msg");
+
+document.getElementById("loginBtn").addEventListener("click", login);
+document.getElementById("registerBtn").addEventListener("click", register);
+
 function login() {
-  document.getElementById("msg").innerText = "Login ainda não configurado";
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      msg.innerText = "✅ Login realizado com sucesso";
+    })
+    .catch(error => {
+      msg.innerText = "❌ " + error.message;
+    });
 }
 
 function register() {
-  document.getElementById("msg").innerText = "Cadastro ainda não configurado";
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      msg.innerText = "✅ Cadastro realizado com sucesso";
+    })
+    .catch(error => {
+      msg.innerText = "❌ " + error.message;
+    });
 }
