946ef16bb9a1c5e566430bbd79d74f5091fb509a
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 04:23:45 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -1,17 +1,11 @@
 import { updateProfile } from "./profiles.js";
 
 document.getElementById("saveProfile").onclick = async () => {
-  const displayName = document.getElementById("displayNameInput").value;
-  const bio = document.getElementById("bioInput").value;
-  const avatarURL = document.getElementById("avatarInput").value;
-  const bannerURL = document.getElementById("bannerInput").value;
+    const displayName = document.getElementById("displayNameInput").value;
+    const bio = document.getElementById("bioInput").value;
+    const avatarURL = document.getElementById("avatarInput").value;
+    const bannerURL = document.getElementById("bannerInput").value;
 
-  await updateProfile({
-    displayName,
-    bio,
-    avatarURL,
-    bannerURL
-  });
-
-  alert("Perfil atualizado!");
+    await updateProfile({ displayName, bio, avatarURL, bannerURL });
+    alert("Perfil atualizado!");
 };
