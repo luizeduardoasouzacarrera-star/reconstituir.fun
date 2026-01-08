6de892e31178423287d3cf22ad6b19217219ceaf
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 04:24:04 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHScM8UbJjH-4aw8gZwClant01Mo0xAkc",
-  authDomain: "reconstituir-b35a3.firebaseapp.com",
-  projectId: "reconstituir-b35a3",
-  storageBucket: "reconstituir-b35a3.firebasestorage.app",
-  messagingSenderId: "238396957236",
-  appId: "1:238396957236:web:ce32659a6be89db1ab2061"
+    apiKey: "AIzaSyCHScM8UbJjH-4aw8gZwClant01Mo0xAkc",
+    authDomain: "reconstituir-b35a3.firebaseapp.com",
+    projectId: "reconstituir-b35a3",
+    storageBucket: "reconstituir-b35a3.firebasestorage.app",
+    messagingSenderId: "238396957236",
+    appId: "1:238396957236:web:ce32659a6be89db1ab2061"
 };
 
 const app = initializeApp(firebaseConfig);
