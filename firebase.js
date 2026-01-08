ea9558d2de45c58fd3055d935d62398eb6a87a5c
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 00:42:21 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHScM8UbJjH-4aw8gZwClant01Mo0xAkc",
+  authDomain: "reconstituir-b35a3.firebaseapp.com",
+  projectId: "reconstituir-b35a3",
+  storageBucket: "reconstituir-b35a3.firebasestorage.app",
+  messagingSenderId: "238396957236",
+  appId: "1:238396957236:web:ce32659a6be89db1ab2061"
+};
+
+// 🔥 AQUI o Firebase é iniciado
+const app = initializeApp(firebaseConfig);
+
+// 🔐 Auth pronto pra usar
+export const auth = getAuth(app);
