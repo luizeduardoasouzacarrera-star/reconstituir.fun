0baf9b1fb67aa1807f1e88afb7989d7b52c41631
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 20:20:20 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCHScM8UbJjH-4aw8gZwClant01Mo0xAkc",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const rtdb = getDatabase(app);
