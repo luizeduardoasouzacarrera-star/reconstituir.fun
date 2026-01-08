bc40cf23d367d4f93da4f9304a5070a8fe7495d0
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 00:05:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+  document.getElementById("msg").innerText = "Login ainda não configurado";
+}
+
+function register() {
+  document.getElementById("msg").innerText = "Cadastro ainda não configurado";
+}
