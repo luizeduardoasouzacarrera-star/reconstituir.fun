b3fd4fa0fe23bdc7867b85439c85be3dd0772bfc
luizeduardoasouzacarrera-star <luizeduardoasouzacarrera@gmail.com>
2026-01-08 21:40:43 -0300

    Create status.js

---
+++ status.js
@@ -0,0 +1,13 @@
+import { auth, rtdb, ref, set, onDisconnect } from "./firebase.js";
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        const statusRef = ref(rtdb, `status/${user.uid}`);
+
+        // Coloca como online
+        set(statusRef, { isOnline: true });
+
+        // Quando desconectar, vira offline
+        onDisconnect(statusRef).set({ isOnline: false });
+    }
+});
